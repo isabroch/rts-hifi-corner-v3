7218c0ec3d06516874109ff72dae18f01edc66b6
Isabella <isabrochweb@gmail.com>
2020-05-02 17:20:31 +0200

    setup react and other tools

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <div>Waiting for Content</div>
+    </React.StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
