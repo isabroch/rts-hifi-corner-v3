482b2e517b967f8fd4666a8606275f11acd15d1d
Isabella Broch <5460278@RTS-365.dk>
2020-05-05 14:03:38 +0200

    do something on search submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,46 +9,55 @@ const SearchBar = () => {
   const [theme] = React.useContext(ThemeContext);
 
   return (
-    <div
-      css={css`
-        display: flex;
-        align-items: baseline;
-        border: 1px solid ${theme.color.neutral};
-        height: 1.75em;
-        width: 25vmax;
-        margin: 0.25em 1.5em;
-        position: relative;
-        background: ${theme.color.light};
-        overflow: hidden;
-      `}
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        alert(
+          `Sorry, search function isn't complete yet! \n Why are you looking for ${searchQuery} anyways?`
+        );
+      }}
     >
-      <input
-        type="search"
-        placeholder="Search entire store here..."
-        value={searchQuery}
-        onChange={e => updateSearchQuery(e.target.value)}
+      <div
         css={css`
-          box-sizing: border-box;
-          padding: 0.5em 0 0.5em 0.5em;
-          height: 100%;
-          background: transparent;
-          border: none;
-          flex: 1;
-          color: ${theme.color.neutralDark};
-        `}
-      />
-      <button
-        css={css`
-          color: ${theme.color.neutral};
-          padding: 0.5em;
+          display: flex;
+          align-items: baseline;
+          border: 1px solid ${theme.color.neutral};
+          height: 1.75em;
+          width: 25vmax;
+          margin: 0.25em 1.5em;
+          position: relative;
           background: ${theme.color.light};
-          border: none;
-          cursor: pointer;
+          overflow: hidden;
         `}
       >
-        <FontAwesomeIcon fixedWidth icon={faSearch} />
-      </button>
-    </div>
+        <input
+          type="search"
+          placeholder="Search entire store here..."
+          value={searchQuery}
+          onChange={e => updateSearchQuery(e.target.value)}
+          css={css`
+            box-sizing: border-box;
+            padding: 0.5em 0 0.5em 0.5em;
+            height: 100%;
+            background: transparent;
+            border: none;
+            flex: 1;
+            color: ${theme.color.neutralDark};
+          `}
+        />
+        <button
+          css={css`
+            color: ${theme.color.neutral};
+            padding: 0.5em;
+            background: ${theme.color.light};
+            border: none;
+            cursor: pointer;
+          `}
+        >
+          <FontAwesomeIcon fixedWidth icon={faSearch} />
+        </button>
+      </div>
+    </form>
   );
 };
 
