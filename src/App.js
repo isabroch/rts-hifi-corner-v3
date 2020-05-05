32f7b17560502cce73b475ab0b7b2e591d3dd7dc
Isabella Broch <5460278@RTS-365.dk>
2020-05-05 10:59:01 +0200

    switch css variables to theme hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
 
@@ -6,22 +6,56 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ShopProducts from "./pages/ShopProducts";
 import ProductDetails from "./pages/ProductDetails";
+import ThemeContext from "./components/ThemeContext";
 import Shop from "./pages/Shop";
 import Brands from "./pages/Brands";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
+  const theme = useState({
+    fontSize: {
+      s: "0.8rem",
+      m: "1rem",
+      l: "2rem",
+      xl: "3rem"
+    },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      bold: 800
+    },
+    color: {
+      required: "#da2a2a",
+      brand: "#a39161",
+      brandDark: "#68542B",
+      dark: "#000000",
+      darkOpacity: "rgba(0, 0, 0, 0.75)",
+      light: "#ffffff",
+      neutralDark: "#444444",
+      neutral: "#acacac",
+      neutralLightOpacity: "rgba(243, 243, 243, 0.8)",
+      neutralLight: "#f3f3f3"
+    },
+    bg: "transparent"
+  });
+
   return (
     <React.StrictMode>
-      <Header />
-      <Router>
-        <Home path="/" />
-        <Brands path="/brands" />
-        <Shop path="/shop" />
-        <ShopProducts path="/shop-products" />
-        <ProductDetails path="/product/:id" />
-      </Router>
-      <Footer />
+      <ThemeContext.Provider value={theme}>
+        <div>
+          <Header />
+          <Router>
+            <Home path="/" />
+            <Brands path="/brands" />
+            <Shop path="/shop" />
+            <ShopProducts path="/shop/products" />
+            <ProductDetails path="/shop/products/:id" />
+            <NotFound default />
+          </Router>
+          <Footer />
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
