7c51d40a4f7876fb36ee4fc51e32c832ce0c6951
Isabella <isabrochweb@gmail.com>
2020-05-06 15:29:39 +0200

    add background

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import { Link } from "@reach/router";
+import Block from "../components/Block";
 
 export default function NotFound() {
   return (
-    <div>
-      Couldn't find the page. Are you lost? <Link to="/">Go Home!</Link>
-    </div>
+    <Block background="white" textColor="black" centered>
+      Couldn't find the page. Are you lost? &nbsp; <Link to="/">Go Home!</Link>
+    </Block>
   );
 }
