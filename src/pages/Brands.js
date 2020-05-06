87757f7d412209f4089c69e47a2e111aa290b7f5
Isabella <isabrochweb@gmail.com>
2020-05-06 18:41:30 +0200

    create brand page header

--- src/pages/Brands.js
+++ src/pages/Brands.js
@@ -1,7 +1,42 @@
 import React from "react";
+import Block from "../components/Block";
+import Logo from "../components/Logo";
+import ThemeContext from "../components/ThemeContext";
+import { css } from "@emotion/core";
+import { Title } from "../components/Title";
 
-const Brands = props => {
-  return <div>Brands</div>;
+const BrandsHeader = () => {
+  const theme = React.useContext(ThemeContext);
+
+  return (
+    <Block background="https://picsum.photos/1920/1080?random=1">
+      <div
+        css={css`
+          max-width: ${theme.maxSize.large};
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          margin: 0 auto;
+          padding: 0 2em;
+          box-sizing: border-box;
+        `}
+      >
+        <Title priority={2}>Top Brands</Title>
+        <Logo />
+      </div>
+    </Block>
+  );
+};
+
+const Brands = () => {
+  const theme = React.useContext(ThemeContext);
+
+  return (
+    <main>
+      <BrandsHeader />
+    </main>
+  );
 };
 
 export default Brands;
