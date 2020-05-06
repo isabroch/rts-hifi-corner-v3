d00331722faa60a3e0d3dec673194b7bb423f4fa
Isabella <isabrochweb@gmail.com>
2020-05-06 16:44:28 +0200

    add styles

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,10 +2,22 @@
 import React from "react";
 import { Link } from "@reach/router";
 import Block from "../components/Block";
+import { css } from "@emotion/core";
+import ThemeContext from "../components/ThemeContext";
 
 export default function NotFound() {
+  const theme = React.useContext(ThemeContext);
+
   return (
-    <Block background="white" textColor="black" centered>
+    <Block
+      background="white"
+      textColor="black"
+      centered
+      size={theme.maxSize.large}
+      css={css`
+        align-items: flex-start;
+      `}
+    >
       Couldn't find the page. Are you lost? &nbsp; <Link to="/">Go Home!</Link>
     </Block>
   );
