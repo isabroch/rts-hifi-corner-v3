f770ce9545882c7d2f8bd638092f96c294ba74bb
Isabella <isabrochweb@gmail.com>
2020-05-06 14:14:36 +0200

    remove extra brackets

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,51 +10,49 @@ const CarouselSlide = props => {
   return (
     <div
       css={css`
-         {
-          display: block;
+        display: block;
+        width: 100%;
+        height: 50vmin;
+        min-height: 200px;
+        margin-right: 1rem;
+        padding: 10vmin 0;
+
+        a {
+          position: relative;
+          height: 100%;
           width: 100%;
-          height: 50vmin;
-          min-height: 200px;
-          margin-right: 1rem;
-          padding: 10vmin 0;
-
-          a {
-            position: relative;
-            height: 100%;
-            width: 100%;
-            display: block;
-            &:hover {
-              filter: brightness(1.1);
-            }
+          display: block;
+          &:hover {
+            filter: brightness(1.1);
           }
+        }
 
-          .carouselTitle {
-            top: 10%;
-            right: 55%;
-            display: block;
-            font-size: ${theme.fontSize.l};
-            text-transform: uppercase;
-            position: absolute;
-            max-width: 12ch;
-            font-weight: ${theme.fontWeight.bold};
-            color: ${theme.color.brand};
-            margin: 0;
-            line-height: 1;
-            z-index: 1;
-            background-color: ${theme.color.neutralLightOpacity};
-            backdrop-filter: blur(5px);
-            text-shadow: 1px 1px 1px ${theme.color.light};
-            box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.25),
-              2px 2px 2px 1px rgba(0, 0, 0, 0.1),
-              6px 6px 8px 2px rgba(0, 0, 0, 0.1);
-            padding: 0.5em;
-          }
+        .carouselTitle {
+          top: 10%;
+          right: 55%;
+          display: block;
+          font-size: ${theme.fontSize.l};
+          text-transform: uppercase;
+          position: absolute;
+          max-width: 12ch;
+          font-weight: ${theme.fontWeight.bold};
+          color: ${theme.color.brand};
+          margin: 0;
+          line-height: 1;
+          z-index: 1;
+          background-color: ${theme.color.neutralLightOpacity};
+          backdrop-filter: blur(5px);
+          text-shadow: 1px 1px 1px ${theme.color.light};
+          box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.25),
+            2px 2px 2px 1px rgba(0, 0, 0, 0.1),
+            6px 6px 8px 2px rgba(0, 0, 0, 0.1);
+          padding: 0.5em;
+        }
 
-          img {
-            height: 100%;
-            width: 100%;
-            object-fit: contain;
-          }
+        img {
+          height: 100%;
+          width: 100%;
+          object-fit: contain;
         }
       `}
     >
