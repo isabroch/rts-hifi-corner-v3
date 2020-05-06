003ae0af3bf35cb7006afb96acf572d89bacf6b8
Isabella <isabrochweb@gmail.com>
2020-05-06 18:41:42 +0200

    create title

---
+++ src/components/Title.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ThemeContext from "./ThemeContext";
+import { css } from "@emotion/core";
+export const Title = ({ children, priority = 0, textColor = "inherit" }) => {
+  const theme = React.useContext(ThemeContext);
+  const HeaderPriority = Number(priority) > 0 ? `h${Number(priority)}` : `span`;
+  const styles = css`
+    color: ${textColor};
+    font-weight: ${theme.fontWeight.bold};
+    font-size: ${theme.fontSize.l};
+    margin: 0.5em 0;
+    text-transform: uppercase;
+    display: block;
+    text-shadow: ${theme.shadow};
+  `;
+  return <HeaderPriority css={styles}>{children}</HeaderPriority>;
+};
